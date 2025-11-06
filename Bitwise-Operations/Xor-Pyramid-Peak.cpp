3799e8a99c17b928f7652323b17cd717a987ec16
Aaryan Sharma <82556028+aaryan-x@users.noreply.github.com>
2025-11-06 22:28:27 +0530

    Update Xor-Pyramid-Peak.cpp

--- Bitwise-Operations/Xor-Pyramid-Peak.cpp
+++ Bitwise-Operations/Xor-Pyramid-Peak.cpp
@@ -1,37 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
- 
+
 const int N = 2e5;
 int power[N];
- 
-void solve(){
-    ll n;
-    cin >> n;
-    vector<ll> arr(n);
-    for(int i=0;i<n;i++){
-        cin >> arr[i];
-    }
-    
-    power[0] = 0;
-    power[1]= 0;
-    power[2]= 1;
-    for(int i=3; i<N;i++){
-      power[i] = (i/2)+ power[i/2];
-    }
-    
-    ll ans = 0;
-    for(int i=0; i<n;i++){
-      ll c1 = power[n-1];
-      ll c2= power[i];
-      ll c3= power[n-i-1];
-      
-      if((c1-c2-c3) <= 0) ans ^= arr[i];
-    }
-    cout<<ans<<endl;
+
+void solve() {
+  ll n;
+  cin >> n;
+  vector<ll> arr(n);
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+
+  power[0] = 0;
+  power[1] = 0;
+  power[2] = 1;
+  for (int i = 3; i < N; i++) {
+    power[i] = (i / 2) + power[i / 2];
+  }
+
+  ll ans = 0;
+  for (int i = 0; i < n; i++) {
+    ll c1 = power[n - 1];
+    ll c2 = power[i];
+    ll c3 = power[n - i - 1];
+
+    if ((c1 - c2 - c3) <= 0) ans ^= arr[i];
+  }
+  cout << ans << endl;
 }
- 
-int main(){
-    solve();
-    return 0;
+
+int main() {
+  solve();
+  return 0;
 }
