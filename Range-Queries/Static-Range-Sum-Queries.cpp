02d664998a28555c434c80cf5eb28a2dd5083558
Aaryan Sharma <82556028+aaryan-x@users.noreply.github.com>
2026-03-01 04:09:28 +0530

    Simplified the code

--- Range-Queries/Static-Range-Sum-Queries.cpp
+++ Range-Queries/Static-Range-Sum-Queries.cpp
@@ -1,28 +1,24 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
-const ll mod = 1e9 + 7;
-
-void solve() {
-  ll n, q;
-  cin >> n >> q;
-  vector<ll> a(n + 1, 0);
-  for (ll i = 1; i <= n; i++) {
-    cin >> a[i];
-    a[i] += a[i - 1];
-  }
-
-  while (q--) {
-    ll x, y;
-    cin >> x >> y;
-    ll rangeSum = a[y] - a[x - 1];
-    cout << rangeSum << endl;
-  }
-}
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  solve();
+  
+  ll n, q;
+  cin >> n >> q;
+  vector <ll> arr(n + 1, 0);
+  for (int i = 1; i <= n; i++) {
+    ll x;
+    cin >> x;
+    arr[i] = x + arr[i - 1];
+  }
+
+  while (q--) {
+    ll l, r;
+    cin >> l >> r;
+    cout << arr[r] - arr[l - 1] << endl;
+  }
   return 0;
 }
